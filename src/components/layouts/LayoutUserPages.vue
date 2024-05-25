866052cc3c1df2244b59acea052143f2860a892e
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-05-25 13:13:17 +0400

    fix: create layoutcontaining background styles and navigation menu  for authenticated pages and use it for profile page

---
+++ src/components/layouts/LayoutUserPages.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HeaderAuthNavigation from "@/components/shared/HeaderAuthNavigation.vue";
+
+const props = defineProps<{
+  name?: string;
+  lang: string;
+  image?: string;
+}>();
+</script>
+<template>
+  <div class="hidden w-full px-16 pt-8 pb-[15rem] xl:flex bg-[#181724]">
+    <section class="w-[14.625rem] mr-[11.5rem]">
+      <HeaderAuthNavigation
+        :name="props.name"
+        :lang="props.lang"
+        :image="props.image"
+      />
+    </section>
+    <section>
+      <slot name="content"></slot>
+    </section>
+  </div>
+</template>
